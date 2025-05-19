e173af9390ffc24ae06bf8c7766a697c9e1ae465
Rainy193 <ktimothy776@gmail.com>
2025-05-19 10:25:52 +0800

    new commit

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -1,9 +1,20 @@
  <?php
  include '../config/controller.php';
 
-$absensi = select(" SELECT * FROM absensi");
+function getCount($status) {
+    global $db;
+    $query = mysqli_query($db, "SELECT COUNT(*) as total FROM absensi WHERE kehadiran = '$status'");
+    $data = mysqli_fetch_assoc($query);
+    return $data['total'];
+}
+$jumlahSiswa  = mysqli_num_rows(mysqli_query($db, "SELECT * FROM absensi"));
+$hadir = getCount('Hadir');
+$sakit = getCount('Sakit');
+$izin  = getCount('Izin');
 
 ?>
+
+
  
  
  
@@ -44,6 +55,100 @@ $absensi = select(" SELECT * FROM absensi");
          <!-- TOP Nav Bar END -->
           
          <!-- Page Content  -->
+
+          <div id="content-page" class="content-page">
+                
+                <div class="container-fluid">
+                <div class="row content-body">
+
+    <!-- Page Heading -->
+                <div class="d-sm-flex align-items-center justify-content-between mb-4">
+                    <h1 class="h3 mb-0 text-gray-800">Dashboard</h1> <!-- Judul halaman dashboard -->
+                    </div>
+
+    <!-- Content Row -->
+    <div class="row">
+
+<!-- Jumlah Hadir -->
+<div class="col-xl-3 col-md-6 mb-4">
+    <div class="card border-left-success shadow h-100 py-2">
+        <div class="card-body">
+            <div class="row no-gutters align-items-center">
+                <div class="col mr-2">
+                    <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
+                        Jumlah Hadir</div>
+                    <div class="h5 mb-0 font-weight-bold text-gray-800">
+                        <?php echo $hadir; ?>
+                    </div>
+                </div>
+                <div class="col-auto">
+                    <!-- <i class="fas fa-check-circle fa-2x text-gray-300"></i> -->
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Jumlah Izin -->
+<div class="col-xl-3 col-md-6 mb-4">
+    <div class="card border-left-primary shadow h-100 py-2">
+        <div class="card-body">
+            <div class="row no-gutters align-items-center">
+                <div class="col mr-2">
+                    <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                        Jumlah Izin</div>
+                    <div class="h5 mb-0 font-weight-bold text-gray-800">
+                        <?php echo $izin; ?>
+                    </div>
+                </div>
+                <div class="col-auto">
+                    <!-- <i class="fas fa-user-clock fa-2x text-gray-300"></i> -->
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Jumlah Sakit -->
+<div class="col-xl-3 col-md-6 mb-4">
+    <div class="card border-left-warning shadow h-100 py-2">
+        <div class="card-body">
+            <div class="row no-gutters align-items-center">
+                <div class="col mr-2">
+                    <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
+                        Jumlah Sakit</div>
+                    <div class="h5 mb-0 font-weight-bold text-gray-800">
+                        <?php echo $sakit; ?>
+                    </div>
+                </div>
+                <div class="col-auto">
+                    <!-- <i class="fas fa-user-injured fa-2x text-gray-300"></i> -->
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Jumlah Alpha -->
+<div class="col-xl-3 col-md-6 mb-4">
+    <div class="card border-left-danger shadow h-100 py-2">
+        <div class="card-body">
+            <div class="row no-gutters align-items-center">
+                <div class="col mr-2">
+                    <div class="text-xs font-weight-bold text-danger text-uppercase mb-1">
+                        Jumlah Siswa</div>
+                    <div class="h5 mb-0 font-weight-bold text-gray-800">
+                        <?php echo $jumlahSiswa; ?>
+                    </div>
+                </div>
+                <div class="col-auto">
+                    <!-- <i class="fas fa-user-times fa-2x text-gray-300"></i> -->
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
 
    <div id="content-page" class="content-page">
             
